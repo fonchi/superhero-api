c9ad00bb5588e6db7a10c15ab4055628148ca3ee
Alfonso Vallone <alfonsovallone@gmail.com>
2021-12-16 20:54:45 -0300

    add service change

--- src/main/java/com/w2m/superhero/service/impl/SuperHeroServiceImpl.java
+++ src/main/java/com/w2m/superhero/service/impl/SuperHeroServiceImpl.java
@@ -1,6 +1,9 @@
 package com.w2m.superhero.service.impl;
 
+import static java.util.Objects.isNull;
+
 import com.w2m.superhero.domain.SuperHero;
+import com.w2m.superhero.exception.NotFoundException;
 import com.w2m.superhero.repository.SuperHeroRepository;
 import com.w2m.superhero.service.SuperHeroService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,9 @@ public class SuperHeroServiceImpl implements SuperHeroService {
   public SuperHero findById(String id) {
 
     SuperHero superHero = superHeroRepository.findById(id);
+    if (isNull(superHero)) {
+      throw new NotFoundException();
+    }
     return superHero;
   }
 }
