81812ab23bf1c4185cadd48ea3042dd0832a3241
Alfonso Vallone <alfonsovallone@gmail.com>
2021-12-16 22:46:04 -0300

    add database configuration

--- src/main/java/com/w2m/superhero/domain/SuperHero.java
+++ src/main/java/com/w2m/superhero/domain/SuperHero.java
@@ -1,16 +1,22 @@
 package com.w2m.superhero.domain;
 
 import java.time.LocalDateTime;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class SuperHero {
 
