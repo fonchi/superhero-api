3ad06b1e0be777335dbb91087ca63ea3be5a270a
Alfonso Vallone <alfonsovallone@gmail.com>
2021-12-19 00:55:35 -0300

    add cache for superhero entity on find operations

--- src/main/java/com/w2m/superhero/SuperheroApplication.java
+++ src/main/java/com/w2m/superhero/SuperheroApplication.java
@@ -2,12 +2,14 @@ package com.w2m.superhero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SuperheroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SuperheroApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SuperheroApplication.class, args);
+  }
 
 }
