0356c1bb237355b735bc8a87f51fef121d67f3a7
Alfonso Vallone <alfonsovallone@gmail.com>
2021-12-20 14:27:27 -0300

    add flyway database migration plugin (#10)

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO superhero (id, name, creation_date, update_date) VALUES (1, 'Spiderman', TIMESTAMP WITH TIME ZONE '2021-12-16 00:00:00', TIMESTAMP WITH TIME ZONE '2021-12-17 00:00:00');
-INSERT INTO superhero (id, name, creation_date, update_date) VALUES (2, 'Superman', TIMESTAMP WITH TIME ZONE '2021-12-16 00:00:00', TIMESTAMP WITH TIME ZONE '2021-12-17 00:00:00');
-INSERT INTO superhero (id, name, creation_date, update_date) VALUES (3, 'Batman', TIMESTAMP WITH TIME ZONE '2021-12-16 00:00:00', TIMESTAMP WITH TIME ZONE '2021-12-17 00:00:00');
-INSERT INTO superhero (id, name, creation_date, update_date) VALUES (4, 'Robin', TIMESTAMP WITH TIME ZONE '2021-12-16 00:00:00', TIMESTAMP WITH TIME ZONE '2021-12-17 00:00:00');
