e0638011a4046c08aedb450dd2f69be2a892643c
Alfonso Vallone <alfonsovallone@gmail.com>
2021-12-22 00:30:52 -0300

    add dto validations and exception handler for argument not valid (#13)

--- src/main/java/com/w2m/superhero/application/dto/SuperheroRequestDto.java
+++ src/main/java/com/w2m/superhero/application/dto/SuperheroRequestDto.java
@@ -1,6 +1,9 @@
 package com.w2m.superhero.application.dto;
 
 import com.w2m.superhero.domain.model.Superhero;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,11 +17,13 @@ import lombok.Setter;
 public class SuperheroRequestDto {
 
   private Long id;
+  @NotNull
+  @NotBlank
+  @Size(min = 1, max = 50, message = "name must be less than 50 characters")
   private String name;
 
   /**
    * Constructor to map dto to entity
-   * @return
    */
   public Superhero toEntity() {
     return Superhero.builder()
